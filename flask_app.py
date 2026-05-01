d99bb0c33684dab89f11e2c63142857347fe6427
nickbs87 <nick_bs87@yahoo.com>
2026-05-01 21:00:21 +0300

    Chapter 3.9: Flask-Moment for client-side time localization

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
     fruits = ['banana', 'apple', 'cherry', 'date', 'elderberry']
-    return render_template('index.html', fruits=fruits)
+    return render_template('index.html', fruits=fruits,
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
 
 @app.route('/users')
 def users():
