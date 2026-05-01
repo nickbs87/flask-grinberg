d66283498415ba413f7e82bd460d96b86d8b14f5
nickbs87 <nick_bs87@yahoo.com>
2026-05-01 12:39:53 +0300

    Chapter 3.5: integrate Flask-Bootstrap with nested template blocks

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
