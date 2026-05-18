d7f659fa098d53bab20d7bea6d553a9463b40d78
nickbs87 <nick_bs87@yahoo.com>
2026-05-18 13:59:34 +0300

    Ch 8: Add password hashing to User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
@@ -7,19 +8,32 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship("User", backref="role")
-    
+
     def __repr__(self):
         return f"<Role {self.name!r}>"
-    
-    
+
+
 class User(db.Model):
     __tablename__= "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     email = db.Column(db.String(120))
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
-    
-    
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
     def __repr__(self):
         return f"<User {self.username!r}>"
 
