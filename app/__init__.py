dde87d33058d6780b4d73b48782994cd982d018c
nickbs87 <nick_bs87@yahoo.com>
2026-05-18 14:03:48 +0300

    Ch 8: Add auth blueprint with login route

--- app/__init__.py
+++ app/__init__.py
@@ -17,22 +17,24 @@ mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
-    
+
     # Φόρτωση config
     app.config.from_object(config[config_name])
-    
+
     # hook for specific env setup
     config[config_name].init_app(app)
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
-    
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     return app
-    
-    
+
