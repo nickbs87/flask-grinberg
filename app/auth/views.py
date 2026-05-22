ab030b6c31b8fb04c39a1842eae276c743a25ed8
nickbs87 <nick_bs87@yahoo.com>
2026-05-22 11:36:30 +0300

    Ch 8: Implement login view with authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,26 @@
-from flask import render_template
-
-from flask_login import login_required
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, login_user
 from .forms import LoginForm
-
 from . import auth
+from ..models import User
+
 
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next_page = request.args.get('next')
+            if next_page is None or not next_page.startswith('/'):
+                next_page = url_for('main.index')
+            return redirect(next_page)
+        else:
+            flash("Invalid Email or Password! Try again")
+
     return render_template('auth/login.html', form=form)
 
 
