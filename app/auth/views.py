bc48a2144eaa5f2ea7a2a602fa686520daa54d17
nickbs87 <nick_bs87@yahoo.com>
2026-05-23 11:19:35 +0300

    Ch 8: Add logout and registration features

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_required, login_user
-from .forms import LoginForm
+from flask_login import login_required, login_user, logout_user
+from .forms import LoginForm, RegistrationForm
 from . import auth
 from ..models import User
+from .. import db
 
 
 
@@ -24,12 +25,27 @@ def login():
     return render_template('auth/login.html', form=form)
 
 
+
 @auth.route('/logout')
 @login_required
 def logout():
-    return render_template('auth/login.html')
+    logout_user()
+    flash("You have been logged out")
+    return redirect(url_for('main.index'))
 
 
-@auth.route('/register')
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    return
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
