247b6bc40ac5d74dd4278c57a28fe36e7d200388
nickbs87 <nick_bs87@yahoo.com>
2026-06-07 10:27:54 +0300

    Ch 10:Fixed typos in the code

--- app/auth/views.py
+++ app/auth/views.py
@@ -79,7 +79,7 @@ def unconfirmed():
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated
+    if current_user.is_authenticated:
         current_user.ping()
         if not current_user.confirmed \
             and request.endpoint \
