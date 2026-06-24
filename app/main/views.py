4c3f27984d151205a1427e70d1145892429ce666
nickbs87 <nick_bs87@yahoo.com>
2026-06-24 21:52:49 +0300

    Ch 12: Add follow, unfollow, followers and followed-by routes with templates

--- app/main/views.py
+++ app/main/views.py
@@ -54,8 +54,6 @@ def edit_post(id):
     return render_template('edit-post.html', form=form)
 
 
-
-
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first()
@@ -75,7 +73,6 @@ def users():
         {'name':'Eleni', 'age':33, 'role':'user'}
     ]
     return render_template('users.html', users=users_list)
-
 
 
 @main.route('/feedback', methods=['GET', 'POST'])
@@ -153,10 +150,75 @@ def edit_profile_admin(id):
     return render_template('edit-profile.html', form=form, user=user)
 
 
+@main.route('/follow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    if current_user == user:
+        flash("You can't follow yourself.")
+        return redirect(url_for('.user', username=username))
+    if current_user.is_following(user):
+        flash('You are already following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash(f"You are now following {username}.")
+    return redirect(url_for('.user', username=username))
 
 
+@main.route('/unfollow/<username>')
+@login_required
+@permission_required(Permission.FOLLOW)
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    if not current_user.is_following(user):
+        flash('You are not following this user.')
+        return redirect(url_for('.user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash(f"You have unfollowed {username}.")
+    return redirect(url_for('.user', username=username))
 
 
+@main.route('/followers/<username>')
+def followers(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followers.paginate(
+        page=page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
+        error_out=False)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp}
+               for item in pagination.items]
+    return render_template('followers.html', user=user, title="Followers of",
+                           endpoint='.followers', pagination=pagination,
+                           follows=follows)
+
+
+@main.route('/followed-by/<username>')
+def followed_by(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('Invalid user.')
+        return redirect(url_for('.index'))
+    page = request.args.get('page', 1, type=int)
+    pagination = user.followed.paginate(
+        page=page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
+        error_out=False)
+    follows = [{'user': item.followed, 'timestamp': item.timestamp}
+               for item in pagination.items]
+    return render_template('followers.html', user=user, title='Followed by',
+                           endpoint='.followed_by', pagination=pagination,
+                           follows=follows)
 
 
 @main.route('/admin')
