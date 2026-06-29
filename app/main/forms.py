fd579ec150cbaf23dd8ca5977c45a2e5e29dca19
nickbs87 <nick_bs87@yahoo.com>
2026-06-29 13:51:04 +0300

    Ch 13: Implement full user commenting system with pagination

--- app/main/forms.py
+++ app/main/forms.py
@@ -63,3 +63,8 @@ class EditProfileAdminForm(FlaskForm):
 class PostForm(FlaskForm):
     body = PageDownField("What's on your mind", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class CommentForm(FlaskForm):
+    body = TextAreaField("Write your comment", validators=[DataRequired()])
+    submit = SubmitField("Submit")
