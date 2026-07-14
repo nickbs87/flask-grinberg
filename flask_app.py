849c64e36f8a0143a4d0ebeedc0498a6fa731055
nickbs87 <nick_bs87@yahoo.com>
2026-07-14 21:21:25 +0300

    Ch 16: Add source code profiling command

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ import click
 from pathlib import Path
 
 
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -47,3 +48,17 @@ def test(coverage):
         COV.html_report(directory=str(covdir))
         print(f'HTML version: file://{covdir}/index.html')
         COV.erase()
+
+@app.cli.command()
+@click.option('--length', default=25,
+              help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,
+              help='Directory where profiler data files are saved.')
+
+def profile(length, profile_dir):
+    """ Start the application under the code profiler. """
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    os.environ.pop('FLASK_RUN_FROM_CLI', None)
+    app.run(debug=False)
