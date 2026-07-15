6b70dd8343ac342ba6e21245f28cafce47d12f50
nickbs87 <nick_bs87@yahoo.com>
2026-07-15 13:42:30 +0300

    Ch 17: Add production email and stream logging handlers

--- config.py
+++ config.py
@@ -32,7 +32,11 @@ class Config:
 
     @staticmethod
     def init_app(app):
-        pass
+        import logging
+        from logging import StreamHandler
+        stream_handler = StreamHandler()
+        stream_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(stream_handler)
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -43,6 +47,31 @@ class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
         f'sqlite:///{base_dir / "data.sqlite"}'
+
+    @staticmethod
+    def init_app(app):
+        Config.init_app(app)
+
+
+        import logging
+        from logging.handlers import SMTPHandler
+
+        credentials = None
+        secure = None
+        if app.config.get('MAIL_USERNAME'):
+            credentials = (app.config['MAIL_USERNAME'],
+                        app.config['MAIL_PASSWORD'])
+            if app.config.get('MAIL_USE_TLS'):
+                secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr=app.config['MAIL_DEFAULT_SENDER'],
+            toaddrs=[app.config['FLASKY_ADMIN']],
+            subject=app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + 'Application Error',
+                    credentials=credentials, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 
